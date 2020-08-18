d422a024cf5530ccb01b878a93f07b8be59be5eb
SuryaPonna <suryaponna0@gmail.com>
2020-08-18 21:31:18 +0530

    Add project files.

---
+++ Group2.Znalytics.GreatOutDoors.PresentationLayer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group4.Znalytics.GreatOutDoors.PresentationLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
